925d530754f719de37da04fe60f11da76df9e8e4
Maurice Kleine <hey@mauricekleine.com>
2022-05-05 20:54:06 +0200

    chore: replace headless ui with radix (#45) (#46)
    * chore: replace headless ui with radix
    
    * chore: uninstall headless ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fade-in 150ms ease-in-out",
+        "slide-from-left": "slide-from-left 150ms ease-in",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        "slide-from-left": {
+          "0%": { opacity: 0, transform: "translate(-50%, 0)" },
+          "100%": { opacity: 1, transform: "translate(0, 0)" },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
 };
