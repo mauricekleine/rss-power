48f17dae79095448068f294e83350ce50f3c03e7
Maurice Kleine <hey@mauricekleine.com>
2022-09-27 20:18:04 +0200

    feat: dedicated resource forms and endpoints

--- app/features/resources/snooze-resource-form.tsx
+++ app/features/resources/snooze-resource-form.tsx
@@ -1,26 +1,43 @@
-import { ClockAfternoon } from "~/features/ui/icon";
+import type { UserResource } from "@prisma/client";
+import { useFetcher } from "@remix-run/react";
 
-import ResourceActionForm from "./resource-action-form";
-import { ResourceActions } from "./types";
+import { IconButton } from "~/features/ui/button";
+import { ClockAfternoon } from "~/features/ui/icon";
+import { Tooltip } from "~/features/ui/tooltip";
 
 type Props = {
   isSnoozed: boolean;
   resourceId: string;
 };
 
-export default function SnoozeResourceForm({ isSnoozed, resourceId }: Props) {
+export default function SnoozeResourceForm({
+  isSnoozed: isSnoozedProp,
+  resourceId,
+}: Props) {
+  const fetcher = useFetcher<{ userResource: UserResource }>();
+
+  const isSnoozed =
+    isSnoozedProp || fetcher.submission?.formData.get("isSnoozed") === "on";
+
   return (
-    <ResourceActionForm
-      action={ResourceActions.SNOOZE}
-      isDisabled={isSnoozed}
-      resourceId={resourceId}
-      tooltipContent={isSnoozed ? "Snoozed" : "Snooze"}
-    >
-      <ClockAfternoon
-        className="text-gray-900"
-        size={18}
-        weight={isSnoozed ? "duotone" : "regular"}
-      />
-    </ResourceActionForm>
+    <fetcher.Form action={`/resources/${resourceId}/snooze`} method="post">
+      <input className="hidden" name="isSnoozed" type="checkbox" value="on" />
+
+      <Tooltip content={isSnoozed ? "Snoozed" : "Snooze"} delayDuration={100}>
+        <span tabIndex={0}>
+          <IconButton
+            isDisabled={isSnoozed}
+            isLoading={fetcher.state === "submitting"}
+            type="submit"
+          >
+            <ClockAfternoon
+              className="text-gray-900"
+              size={18}
+              weight={isSnoozed ? "duotone" : "regular"}
+            />
+          </IconButton>
+        </span>
+      </Tooltip>
+    </fetcher.Form>
   );
 }
